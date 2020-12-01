e142d182d77d55555f53d57282509037bef18bee
Alvaro Flores Farfan <alvaro.fafy98@gmail.com>
2020-11-30 23:37:48 -0400

    tablero estatico

--- app.rb
+++ app.rb
@@ -13,15 +13,40 @@ post '/juego' do
 end
 post '/principiante' do
     $tablero = Tablero.new(9,9)
-    erb :principiante
+    erb :colocarMinas
 end
 post '/personalizado' do
     erb :personalizado
 end
 
-post '/seleccionar' do
-    $posX=params[:x]
-    $posY=params[:y]
-    $respuesta=$tablero.clic($posX,$posY)
-    erb :seleccionarCasilla
+post '/marcarMinas' do
+    @posX=params[:x].to_i
+    @posY=params[:y].to_i
+    @res=$tablero.agregarMinas(@posX,@posY)
+    erb :colocarMinas
+end 
+
+post '/comenzarJuego' do
+    $tablero.establecerNumerosEnTablero()
+    erb :tablero
+end
+
+post '/marcar' do
+    @posX=params[:x].to_i
+    @posY=params[:y].to_i
+    @res=$tablero.marcarCasillaTablero(@posX,@posY)
+    
+    if @res==-3
+        erb :finDelJuego
+    else
+        if $tablero.gano
+            erb :gano
+        else
+            erb :tablero
+        end
+    end
+end
+
+post '/volverAjugar' do
+    erb :jugar
 end
